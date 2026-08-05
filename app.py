88f2633b93b4f40bf27ef3734a9f9fe3aa69c0c6
MOHAMED RAIHAAN M <278937498+Mohamed-Raihaan@users.noreply.github.com>
2026-08-05 20:05:18 +0530

    checking the CI/CD

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Contoso retail Production - Cloud Engineer Version</h1><p>Application deployed successfully By RAVEN</p>"
+    return "<h1>Contoso retail Production - Cloud Engineer Arc </h1><p>Application deployed successfully By RAVEN</p>"
 @app.route("/health")
 def health():
     return {
